51c0b53a89c199099b14f6a93ba4de4b57c1658b
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-01 22:29:36 +0100

    optimized solution

--- day1/Program.fs
+++ day1/Program.fs
@@ -10,15 +10,15 @@ let rec part1 sum = function
     | x :: xs ->
         if Seq.contains (sum - x) xs then x * (sum - x) else (part1 sum xs)
 
-let rec part2 = function
+let rec part2 sum = function
     | [] -> 0
     | x :: xs ->
         match part1 (2020 - x) xs with
-        | 0 -> part2 xs
+        | 0 -> part2 sum xs
         | y -> x * y 
 
 [<EntryPoint>]
 let main _ =
     printfn "Part 1: %i" (part1 2020 numbers)
-    printfn "Part 2: %i" (part2 numbers)
+    printfn "Part 2: %i" (part2 2020 numbers)
     0
