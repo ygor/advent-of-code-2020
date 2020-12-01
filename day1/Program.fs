ab32fd272cc5a3420d7e1d930934dc689fb27d36
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-01 14:13:43 +0100

    simplify

--- day1/Program.fs
+++ day1/Program.fs
@@ -26,12 +26,12 @@ let main argv =
         |> pairs
         |> Seq.find (fun (a, b) -> a + b = 2020)
 
-    printfn "Part 1: sum = %i" (a * b)
+    printfn "Part 1: %i" (a * b)
 
     let (a, b, c) =
         numbers
         |> triplets
         |> Seq.find (fun (a, b, c) -> a + b + c = 2020)
 
-    printfn "Part 2: sum = %i" (a * b * c)
+    printfn "Part 2: %i" (a * b * c)
     0
