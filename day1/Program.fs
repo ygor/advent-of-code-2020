fcef8c2733e0abbc0da6b02689c64529fe8a4f5e
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-01 13:38:47 +0100

    simplify

--- day1/Program.fs
+++ day1/Program.fs
@@ -10,7 +10,7 @@ let rec pairs lst =
     | [] -> []
     | x :: xs ->
         xs
-        |> List.map (fun elem -> (x, elem))
+        |> List.map (fun elem -> x, elem)
         |> List.append (pairs xs)
 
 let rec triplets lst =
@@ -26,16 +26,14 @@ let main argv =
     let (a, b) =
         numbers
         |> pairs
-        |> Seq.filter (fun (a, b) -> a + b = 2020)
-        |> Seq.head
+        |> Seq.find (fun (a, b) -> a + b = 2020)
 
     printfn "Part 1: sum: %i" (a * b)
 
     let (a, b, c) =
         numbers
         |> triplets
-        |> Seq.filter (fun (a, b, c) -> a + b + c = 2020)
-        |> Seq.head
+        |> Seq.find (fun (a, b, c) -> a + b + c = 2020)
 
     printfn "Part 2: sum: %i" (a * b * c)
     0
