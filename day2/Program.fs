97befb744d0da179f0e34126a4ef300e65835927
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-02 09:52:54 +0100

    day2

---
+++ day2/Program.fs
@@ -0,0 +1,39 @@
+﻿open System.IO
+open Extensions
+
+type Policy = int * int * char
+type Password = string
+
+let parseLine (line: string): Policy * Password =
+    match line with
+    | Regex "(\d+?)-(\d+?) ([a-z]+): (\w+)" [ min; max; letter; password ] -> (int min, int max, char letter), password
+    | x -> failwithf "Invalid instruction: %A" x
+
+let list =
+    File.ReadAllLines("input.txt")
+    |> Seq.map parseLine
+
+let rentalValidator ((min, max, letter): Policy, password: Password) =
+    let count =
+        password
+        |> Seq.filter (fun c -> c = letter)
+        |> Seq.length
+
+    count >= min && count <= max
+
+let tobogganValidator ((pos1, pos2, letter): Policy, password: Password) =
+    let matches =
+        [ password.[pos1 - 1]
+          password.[pos2 - 1] ]
+        |> Seq.filter ((=) letter)
+
+    Seq.length matches = 1
+
+let validPasswords validator =
+    list |> Seq.filter validator |> Seq.length
+
+[<EntryPoint>]
+let main _ =
+    printfn "Part 1: %i" (validPasswords rentalValidator)
+    printfn "Part 2: %i" ((validPasswords tobogganValidator))
+    0
