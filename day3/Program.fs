6682b76b61fd4a43bd98110f75a76ac159c1568b
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-04 00:25:09 +0100

    day3

---
+++ day3/Program.fs
@@ -0,0 +1,26 @@
+﻿open System.IO
+
+let lines = File.ReadAllLines("input.txt")
+
+let trees (lines: string seq) (right, down) =
+    lines
+    |> Seq.mapi (fun i line -> i, line)
+    |> Seq.filter (fun (i, _) -> i % down = 0)
+    |> Seq.mapi (fun j (_, line) -> if line.[(right * j) % line.Length] = '#' then 1 else 0)
+    |> Seq.sum
+
+let traverse slopes =
+    slopes |> Seq.map (trees lines) |> Seq.reduce (*)
+
+[<EntryPoint>]
+let main _ =
+    printfn "Part 1: %i" (traverse [ (3, 1) ])
+
+    printfn
+        "Part 2: %d"
+        (traverse [ (1, 1)
+                    (3, 1)
+                    (5, 1)
+                    (7, 1)
+                    (1, 2) ])
+    0
