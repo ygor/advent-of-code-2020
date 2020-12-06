0685c508c4bbbd40400f8f687b001343e1df06e4
Ygor Geurts <Ygor.Geurts@rtl.nl>
2020-12-06 08:23:53 +0100

    day6

---
+++ day6/Program.fs
@@ -0,0 +1,26 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+open Extensions
+
+let anyoneYes input =
+    Regex.Replace(input, "\n", "")
+    |> Set.ofSeq
+    |> Seq.length    
+
+let allYes input =
+    input
+    |> String.split "\n"
+    |> Seq.map Set.ofSeq
+    |> Seq.reduce Set.intersect
+    |> Seq.length
+
+let count policy =
+    File.ReadAllText("input.txt")
+    |> String.split "\n\n"
+    |> Seq.sumBy policy
+
+[<EntryPoint>]
+let main _ =
+    printfn "Part 1: %i" (count anyoneYes)
+    printfn "Part 2: %i" (count allYes)
+    0
