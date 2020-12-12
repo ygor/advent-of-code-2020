3da165ab067786d8435288003778d6265054f5df
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-12 22:45:09 +0100

    remove unused code

--- day11/Extensions.fs
+++ day11/Extensions.fs
@@ -1,8 +1,4 @@
 module Extensions
-
-module Map =
-  let keys (m: Map<'Key, 'T>) =
-    Map.fold (fun keys key _ -> key::keys) [] m
     
 module Array2D =
     let toSeq array =
@@ -11,18 +7,3 @@ module Array2D =
                 for c in 0 .. (Array2D.length2 array - 1) do
                     yield (r, c), array.[r, c]
         }
-
-module List =
-    let apply (fList: ('a->'b) list) (xList: 'a list)  = 
-        [ for f in fList do
-          for x in xList do
-              yield f x ]    
-    
-    let (<*>) = apply
-
-    let (<!>) = List.map        
-
-    let unpack2 list =
-        match list with
-        | [ a; b ] -> a, b
-        | _ -> failwithf "Tried to unpack2 list without exactly 2 elements: %A" list
