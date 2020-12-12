473ea2c9f0c7b77be4f88070e86a2c88d4cd83b4
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-13 00:12:25 +0100

    formatting

--- day12/Program.fs
+++ day12/Program.fs
@@ -9,37 +9,38 @@ let rotate (x, y) degrees =
     (x * (Math.Cos radians) - y * (Math.Sin radians) |> Math.Round,
      x * (Math.Sin radians) + y * (Math.Cos radians) |> Math.Round)
 
-let move1 =
-    input
-    |> Seq.fold (fun (x, y, dir) instruction ->
-        match instruction with
-        | Regex "N(\d+)" [ units ] -> (x, y + int units, dir)
-        | Regex "S(\d+)" [ units ] -> (x, y - int units, dir)
-        | Regex "E(\d+)" [ units ] -> (x + int units, y, dir)
-        | Regex "W(\d+)" [ units ] -> (x - int units, y, dir)
-        | Regex "L(\d+)" [ degrees ] -> (x, y, rotate dir (double degrees))
-        | Regex "R(\d+)" [ degrees ] -> (x, y, rotate dir (-1.0 * double degrees))
-        | Regex "F(\d+)" [ units ] -> (x + int units * int (fst dir), y + int units * int (snd dir), dir)
-        | x -> failwithf "Invalid instruction %s" x) (0, 0, (1.0, 0.0))
+let manhattan (x: float, y: float) = int (Math.Abs x + Math.Abs y)
 
-let move2 =
+let part1 =
     input
-    |> Seq.fold (fun (x, y, (wx, wy)) instruction ->
+    |> Seq.fold (fun ((x, y), dir) instruction ->
         match instruction with
-        | Regex "N(\d+)" [ units ] -> (x, y, (wx, wy + float units))
-        | Regex "S(\d+)" [ units ] -> (x, y, (wx, wy - float units))
-        | Regex "E(\d+)" [ units ] -> (x, y, (wx + float units, wy))
-        | Regex "W(\d+)" [ units ] -> (x, y, (wx - float units, wy))
-        | Regex "F(\d+)" [ units ] -> (x + float units * wx, y + float units * wy, (wx, wy))
-        | Regex "L(\d+)" [ degrees ] -> (x, y, rotate (wx, wy) (float degrees))
-        | Regex "R(\d+)" [ degrees ] -> (x, y, rotate (wx, wy) (-1.0 * float degrees))
-        | x -> failwithf "Invalid instruction %s" x) (0.0, 0.0, (10.0, 1.0))
+        | Regex "N(\d+)" [ units ] -> (x, y + float units), dir
+        | Regex "S(\d+)" [ units ] -> (x, y - float units), dir
+        | Regex "E(\d+)" [ units ] -> (x + float units, y), dir
+        | Regex "W(\d+)" [ units ] -> (x - float units, y), dir
+        | Regex "L(\d+)" [ degrees ] -> (x, y), rotate dir (double degrees)
+        | Regex "R(\d+)" [ degrees ] -> (x, y), rotate dir (-1.0 * double degrees)
+        | Regex "F(\d+)" [ units ] -> (x + float units * fst dir, y + float units * snd dir), dir
+        | x -> failwithf "Invalid instruction %s" x) ((0.0, 0.0), (1.0, 0.0))
+    |> (fst >> manhattan)
+
+let part2 =
+    input
+    |> Seq.fold (fun ((x, y), (wx, wy)) instruction ->
+        match instruction with
+        | Regex "N(\d+)" [ units ] -> (x, y), (wx, wy + float units)
+        | Regex "S(\d+)" [ units ] -> (x, y), (wx, wy - float units)
+        | Regex "E(\d+)" [ units ] -> (x, y), (wx + float units, wy)
+        | Regex "W(\d+)" [ units ] -> (x, y), (wx - float units, wy)
+        | Regex "F(\d+)" [ units ] -> (x + float units * wx, y + float units * wy), (wx, wy)
+        | Regex "L(\d+)" [ degrees ] -> (x, y), rotate (wx, wy) (float degrees)
+        | Regex "R(\d+)" [ degrees ] -> (x, y), rotate (wx, wy) (-1.0 * float degrees)
+        | x -> failwithf "Invalid instruction %s" x) ((0.0, 0.0), (10.0, 1.0))
+    |> ( fst >> manhattan)
 
 [<EntryPoint>]
 let main _ =
-    let (x1, y1, _) = move1
-    printfn "Part1: %i" (Math.Abs x1 + Math.Abs y1)
-    
-    let (x2, y2, _) = move2
-    printfn "Part2: %i" (Math.Abs x2 + Math.Abs y2 |> int)    
+    printfn "Part1: %i" part1
+    printfn "Part2: %i" part2    
     0
