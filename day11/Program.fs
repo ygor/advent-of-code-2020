1fb89b011fcd3dc9857387c8bd3949608983eb88
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-12 20:07:25 +0100

    simplify

--- day11/Program.fs
+++ day11/Program.fs
@@ -1,5 +1,4 @@
-﻿open System
-open System.IO
+﻿open System.IO
 open Extensions
 
 let input =
@@ -17,13 +16,13 @@ let directions =
     List.allPairs [ -1 .. 1 ] [ -1 .. 1 ]
     |> List.except [ (0, 0) ]
 
-let isValidCoordinate area (x, y) =
+let inArea area (x, y) =
     x >= 0 && x < (Array2D.length1 area) && y >= 0 && y < (Array2D.length2 area)
 
 let adjacents (x, y) area =
     directions
     |> List.map (fun (a, b) -> (x + a, y + b))
-    |> List.filter (isValidCoordinate area)
+    |> List.filter (inArea area)
 
 let print area =
     [ 0 .. (Array2D.length2 area - 1) ]
@@ -33,28 +32,21 @@ let print area =
         |> Array.reduce (+)
         |> printfn "%s")
 
-let allSeatsEmpty positions =
-    positions
-    |> List.forall (fun pos -> pos = 'L' || pos = '.')
+let onlyEmptySeats positions =
+    List.forall (fun pos -> pos = 'L' || pos = '.') positions
 
 let atLeastOccupied n positions =
-    positions
-    |> List.filter ((=) '#')
-    |> List.length
-    |> (<) (n - 1)
-
-let next area rule =
-    Array2D.init (Array2D.length1 area) (Array2D.length2 area) (rule area)
-
-let rec run area rule =
-    let area' = next area rule
-    if area = area' then area else run area' rule
+    (List.filter ((=) '#') positions |> List.length) >= n
 
 let numOccupiedSeats area =
     area
     |> Array2D.toSeq
     |> Seq.filter (snd >> (=) '#')
     |> Seq.length
+
+let rec evolve area rule =
+    let area' = Array2D.init (Array2D.length1 area) (Array2D.length2 area) (rule area)
+    if area = area' then area else evolve area' rule
 
 //--- Part 1
 
@@ -64,7 +56,7 @@ let rule1 area x y =
         |> List.map (fun (x, y) -> area.[x, y])
 
     match area.[x, y] with
-    | 'L' -> if allSeatsEmpty positions then '#' else 'L'
+    | 'L' -> if onlyEmptySeats positions then '#' else 'L'
     | '#' -> if atLeastOccupied 4 positions then 'L' else '#'
     | x -> x
 
@@ -72,10 +64,10 @@ let rule1 area x y =
 
 let rec firstVisibleSeat area (x, y) (dx, dy) =
     let x', y' = x + dx, y + dy
-    if isValidCoordinate area (x', y') then
+    if inArea area (x', y') then
         if area.[x', y'] <> '.' then Some (x', y') else firstVisibleSeat area (x', y') (dx, dy)
     else
-        None    
+        None
 
 let seats area (x, y) =
     directions
@@ -89,17 +81,17 @@ let rule2 area x y =
     let seats = seats area (x, y)   
 
     match area.[x, y] with
-    | 'L' -> if allSeatsEmpty seats then '#' else 'L'
+    | 'L' -> if onlyEmptySeats seats then '#' else 'L'
     | '#' -> if atLeastOccupied 5 seats then 'L' else '#'
     | x -> x
 
 [<EntryPoint>]
 let main _ =
-    let area' = run area rule1
+    let area' = evolve area rule1
     print area' |> ignore
     printfn "Part 1: %A" (numOccupiedSeats area')
 
-    let area'' = run area rule2
+    let area'' = evolve area rule2
     print area'' |> ignore
     printfn "Part 2: %A" (numOccupiedSeats area'')
     0
