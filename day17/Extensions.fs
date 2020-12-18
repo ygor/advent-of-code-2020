afb416a876e82dc3656fec05b9e98d70a547c79b
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-18 23:38:51 +0100

    clean solution!

--- day17/Extensions.fs
+++ day17/Extensions.fs
@@ -1,35 +1,14 @@
 module Extensions
-
-open System.Text.RegularExpressions
-
-let (|Regex|_|) pattern input =
-    let m = Regex.Match(input, pattern)
-    if m.Success
-    then Some(List.tail [ for g in m.Groups -> g.Value ])
-    else None
     
 module List =
-    let apply (fList: ('a->'b) list) (xList: 'a list)  = 
-        [ for f in fList do
-          for x in xList do
-              yield f x ]
-
-    let (<*>) = apply
-
-module Tuple =
-    let fst3 (x, _, _) = x
-    let snd3 (_, y, _) = y
-    let trd3 (_, _, z) = z
-
-    let fst4 (x, _, _, _) = x
-    let snd4 (_, y, _, _) = y
-    let trd4 (_, _, z, _) = z    
-    let fth4 (_, _, _, w) = w
-    
-    let flatten3 (x, (y, z)) = x, y, z
-
-    let flatten4 (x, (y, (z, w))) = x, y, z, w
-    
+    let rec cartesian list =
+        match list with
+        | h::[] ->
+            List.fold (fun acc elem -> [elem]::acc) [] h
+        | h::t ->
+            List.fold (fun acc' elem' ->
+                (List.fold (fun acc elem -> (elem::elem')::acc) [] h) @ acc') [] (cartesian t)
+        | _ -> []
         
-module String =
-    let split (sep: string) (value: string) = value.Split(sep) |> List.ofArray
+module Tuple =
+    let sum (a, b) = a + b    
--- day17/Program.fs
+++ day17/Program.fs
@@ -1,246 +1,59 @@
 ﻿open System.IO
 open Extensions
 open Extensions.Tuple
-open Microsoft.VisualBasic.FileIO
 
-type Point3 = int * int * int
-type Point4 = int * int * int * int
+type Point = int list
+type Cubes = Set<Point>
 
-type Tube =
-    | Active
-    | InActive
-
-type Grid<'a when 'a : comparison> = Map<'a, Tube>
-
-let minBy f list =
-    list |> List.minBy (fst >> f) |> (fst >> f)
-
-let maxBy f list =
-    list |> List.maxBy (fst >> f) |> (fst >> f)
-
-let neighbours3 (x: int, y: int, z: int) =
-    [ -1; 0; 1 ]
-    |> List.allPairs [ -1; 0; 1 ] 
-    |> List.allPairs [ -1; 0; 1 ]
-    |> List.map flatten3
-    |> List.filter (fun (dx, dy, dz) -> (dx, dy, dz) <> (0, 0, 0))
-    |> List.map (fun (dx, dy, dz) -> x + dx, y + dy, z + dz)
-
-let neighbours4 (x: int, y: int, z: int, w: int) =
-    [ -1; 0; 1 ]
-    |> List.allPairs [ -1; 0; 1 ] 
-    |> List.allPairs [ -1; 0; 1 ]
-    |> List.allPairs [ -1; 0; 1 ]
-    |> List.map flatten4
-    |> List.filter (fun (dx, dy, dz, dw) -> (dx, dy, dz, dw) <> (0, 0, 0, 0))
-    |> List.map (fun (dx, dy, dz, dw) -> x + dx, y + dy, z + dz, w + dw)
-
-let boundaries3 (grid: Grid<Point3>) =
-    let values = grid |> Map.toList
-
-    let xMax, xMin =
-        values |> maxBy fst3, values |> minBy fst3
-
-    let yMax, yMin =
-        values |> maxBy snd3, values |> minBy snd3
-
-    let zMax, zMin =
-        values |> maxBy trd3, values |> minBy trd3
-
-    [ zMin - 1 .. zMax + 1 ]
-    |> List.allPairs [ yMin - 1 .. yMax + 1 ]
-    |> List.allPairs [ xMin - 1 .. xMax + 1 ]
-    |> List.map flatten3
-    |> List.except (grid |> Map.toList |> List.map (fst))
-    
-let boundaries4 (grid: Grid<Point4>) =
-    let values = grid |> Map.toList
-
-    let xMax, xMin =
-        values |> maxBy fst4, values |> minBy fst4
-
-    let yMax, yMin =
-        values |> maxBy snd4, values |> minBy snd4
-
-    let zMax, zMin =
-        values |> maxBy trd4, values |> minBy trd4
-
-    let wMax, wMin =
-        values |> maxBy fth4, values |> minBy fth4
-        
-    [ wMin - 1 .. wMax + 1 ]
-    |> List.allPairs [ zMin - 1 .. zMax + 1 ]
-    |> List.allPairs [ yMin - 1 .. yMax + 1 ]
-    |> List.allPairs [ xMin - 1 .. xMax + 1 ]
-    |> List.map flatten4
-    |> List.except (grid |> Map.toList |> List.map (fst))
-    
-
-let tubes<'a when 'a : comparison> (grid: Grid<'a>) points =
-    points
-    |> List.map (fun point -> point, (if grid.ContainsKey(point) then grid.[point] else InActive))
-
-let rec extend3 grid =
-    let boundaries = boundaries3 grid |> tubes grid
-
-    let numActives =
-        boundaries
-        |> List.filter (snd >> (=) Active)
-        |> List.length
-
-    let grid' =
-        (grid |> Map.toList) @ boundaries |> Map.ofList
-
-    if (numActives > 0) then extend3 grid' else grid'
-
-let rec extend4 grid =
-    let boundaries = boundaries4 grid |> tubes grid
-
-    let numActives =
-        boundaries
-        |> List.filter (snd >> (=) Active)
-        |> List.length
-
-    let grid' =
-        (grid |> Map.toList) @ boundaries |> Map.ofList
-
-    if (numActives > 0) then extend4 grid' else grid'
-
-let numActiveNeighbours3 grid point =
-    neighbours3 point
-    |> tubes grid
-    |> List.filter (snd >> (=) Active)
-    |> List.length
-
-let numActiveNeighbours4 grid point =
-    neighbours4 point
-    |> tubes grid
-    |> List.filter (snd >> (=) Active)
-    |> List.length
-
-let cycle3 (grid: Grid<Point3>) =
-    let grid' = extend3 grid
-
-    grid'
-    |> Map.toList
-    |> Seq.fold (fun (result: Grid<Point3>) (point, state) ->
-        let num = numActiveNeighbours3 grid' point
-
-        let value =
-            match state with
-            | Active -> if num = 2 || num = 3 then Active else InActive
-            | InActive -> if num = 3 then Active else InActive
-
-        result.Add(point, value)) Map.empty<Point3, Tube>
-
-let cycle4 (grid: Grid<Point4>) =
-    let grid' = extend4 grid
-
-    grid'
-    |> Map.toList
-    |> Seq.fold (fun (result: Grid<Point4>) (point, state) ->
-        let num = numActiveNeighbours4 grid' point
-
-        let value =
-            match state with
-            | Active -> if num = 2 || num = 3 then Active else InActive
-            | InActive -> if num = 3 then Active else InActive
-
-        result.Add(point, value)) Map.empty<Point4, Tube>
-
-
-let print3 (grid: Grid<Point3>) z =
-    let values = grid |> Map.toList
-
-    let xMax, xMin =
-        values |> maxBy fst3, values |> minBy fst3
-
-    let yMax, yMin =
-        values |> maxBy snd3, values |> minBy snd3
-
-    [ yMin .. yMax ]
-    |> Seq.iter (fun y ->
-        [ xMin .. xMax ]
-        |> List.map (fun x ->
-            match grid.[x, y, z] with
-            | Active -> '#'
-            | InActive -> '.')
-        |> List.toArray
-        |> System.String
-        |> printfn "%s")
-
-let print4 (grid: Grid<Point4>) z w =
-    let values = grid |> Map.toList
-
-    let xMax, xMin =
-        values |> maxBy fst4, values |> minBy fst4
-
-    let yMax, yMin =
-        values |> maxBy snd4, values |> minBy snd4
-
-    [ yMin .. yMax ]
-    |> Seq.iter (fun y ->
-        [ xMin .. xMax ]
-        |> List.map (fun x ->
-            match grid.[x, y, z, w] with
-            | Active -> '#'
-            | InActive -> '.')
-        |> List.toArray
-        |> System.String
-        |> printfn "%s")
-
-let grid3 =
-    let data =
-        File.ReadAllLines("input.txt") |> List.ofArray
-
-    data
+let cubes =
+    File.ReadAllLines("input.txt")
+    |> List.ofArray
     |> List.indexed
-    |> List.fold (fun grid (y, line) ->
+    |> List.fold (fun cubes (y, line) ->
         line
         |> List.ofSeq
         |> List.indexed
-        |> List.fold (fun (grid': Grid<Point3>) (x, c) ->
-            let value =
-                match c with
-                | '.' -> InActive
-                | '#' -> Active
-                | x -> failwithf "Invalid initial state: %c" x
+        |> List.fold (fun (cubes': Cubes) (x, c) ->
+            match c with
+            | '.' -> cubes'
+            | '#' -> cubes'.Add([ x; y; 0 ])
+            | x -> failwithf "Invalid initial state: %c" x) cubes) Set.empty
 
-            grid'.Add((x, y, 0), value)) grid) Map.empty<Point3, Tube>
+let dxs (cubes: Cubes) =
+    let dim = Set.maxElement cubes |> List.length
+    List.init dim (fun i -> [ -1; 0; 1 ])
+    |> List.cartesian
 
-let grid4 =
-    let data =
-        File.ReadAllLines("input.txt") |> List.ofArray
+let neighbours (cubes: Cubes) point =
+    dxs cubes
+    |> List.fold (fun (neighbours': Cubes) dx ->
+        List.zip point dx
+        |> List.map sum
+        |> neighbours'.Add) Set.empty
+    |> Set.filter (fun neighbour -> neighbour <> point && cubes.Contains neighbour)
 
-    data
-    |> List.indexed
-    |> List.fold (fun grid (y, line) ->
-        line
-        |> List.ofSeq
-        |> List.indexed
-        |> List.fold (fun (grid': Grid<Point4>) (x, c) ->
-            let value =
-                match c with
-                | '.' -> InActive
-                | '#' -> Active
-                | x -> failwithf "Invalid initial state: %c" x
+let cycle (cubes: Cubes) =
+    let dim = Set.maxElement cubes |> List.length
+    let dxs = List.init dim (fun i -> [ -1; 0; 1 ]) |> List.cartesian
 
-            grid'.Add((x, y, 0, 0), value)) grid) Map.empty<Point4, Tube>
+    cubes
+    |> Set.fold (fun points point ->
+        dxs
+        |> List.fold (fun (points': Cubes) dx ->
+            List.zip point dx |> List.map sum |> points'.Add) points) Set.empty
+    |> Set.fold (fun (cubes': Cubes) point ->
+        let num = neighbours cubes point |> Set.count
+        if num = 3 then cubes'.Add point
+        elif num = 2 && cubes.Contains point then cubes'.Add point
+        else cubes') Set.empty
 
-
-let boot<'a when 'a : comparison> (grid: Grid<'a>) cycle =
-    let grid'' =
-        [ 0 .. 5 ]
-        |> List.fold (fun grid' _ -> cycle grid') grid
-
-    grid''
-    |> Map.toSeq
-    |> Seq.filter (fun (_, value) -> value = Active)
-    |> Seq.length
+let boot cubes =
+    [ 0 .. 5 ]
+    |> List.fold (fun cubes' _ -> cycle cubes') cubes
+    |> Set.count
 
 [<EntryPoint>]
 let main _ =
-    print3 grid3 0
-    printfn "Part 1: %A" (boot grid3 cycle3)
-    printfn "Part 2: %A" (boot grid4 cycle4)
+    printfn "Part 1: %A" (boot cubes)
+    printfn "Part 2: %A" (boot (cubes |> Set.map (fun point -> point @ [0])))
     0
