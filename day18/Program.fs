4d511c0bb684ed71b19c807f1e17b1fe831baa53
Ygor Geurts <Ygor.Geurts@rtl.nl>
2020-12-23 12:54:46 +0100

    simplify

--- day18/Program.fs
+++ day18/Program.fs
@@ -12,46 +12,47 @@ let input =
     |> List.ofArray
     |> List.map (fun line ->
         let collection = Regex.Matches(line, "\d+|[+*()]")
-        seq { for m in collection do yield m.Value } |> Seq.toList)
+        seq {
+            for m in collection do
+                yield m.Value
+        }
+        |> Seq.toList)
 
 let rec parse (stack: Expression list) (input: string list) =
     match input with
     | "(" :: xs -> parse (Leaf "(" :: stack) xs
     | ")" :: xs ->
         let left, right = List.span ((<>) (Leaf "(")) stack
-        let stack' = if not right.IsEmpty && List.head right = Leaf "(" then List.tail right else right
-        parse (Node (List.rev left) :: stack') xs
+        let stack' =
+            if not right.IsEmpty && List.head right = Leaf "("
+            then List.tail right
+            else right
+        parse (Node(List.rev left) :: stack') xs
     | x :: xs -> parse (Leaf x :: stack) xs
-    | [] -> List.rev stack    
-    
-let rec solver1 (expressions: Expression list) =
-    match expressions with
-    | Leaf lf :: [] -> BigInteger.Parse lf
-    | Node lst :: [] -> solver1 lst
-    | left :: op :: right :: tail ->
-        let left', right' = solver1 [left], solver1 [right]
-        let op' = if op = Leaf "*" then (*) else (+)
-        solver1 (Leaf (string (op' left' right')) :: tail)
-    | x -> failwithf "Invalid expression: %A" x
+    | [] -> List.rev stack
 
-let rec solver2 (expressions: Expression list) =
+let rec solver (precedence: Map<string, int>) (expressions: Expression list) =
     match expressions with
     | Leaf lf :: [] -> BigInteger.Parse lf
-    | Node lst :: [] -> solver2 lst
+    | Node lst :: [] -> solver precedence lst
     | left :: op :: right :: [] ->
-        let left', right' = solver2 [left], solver2 [right]
+        let left', right' = solver precedence [ left ], solver precedence [ right ]
         let op' = if op = Leaf "*" then (*) else (+)
-        solver2 (Leaf (string (op' left' right')) :: [])
-    | left :: op1 :: right :: op2 :: tail ->
-        if op1 = Leaf "*" && op2 = Leaf "+" then solver2 (left :: op1 :: [Leaf ( string (solver2 (right :: op2 :: tail)) )])
-        else solver2 (Leaf (string (solver2 (left :: op1 :: [right]))) :: op2 :: tail)
-    | x -> failwithf "Invalid expression: %A" x
+        solver precedence [ Leaf <| string (op' left' right') ]
+    | left :: (Leaf op1) :: right :: (Leaf op2) :: tail ->
+        if precedence.[op2] > precedence.[op1] then
+            let sub = solver precedence (right :: Leaf op2 :: tail)
+            solver precedence [ left; Leaf op1; Leaf (string sub) ]
+        else
+            let sub = solver precedence [left; Leaf op1; right ]
+            solver precedence (Leaf (string sub) :: Leaf op2 :: tail)
+    | x -> failwithf "Invalid expression list: %A" x
 
-let solve input solver =
-    input |> List.sumBy (parse List.empty >> solver)
+let solve input precedence =
+    input |> List.sumBy (parse List.empty >> (solver precedence))
 
 [<EntryPoint>]
 let main _ =
-    printfn "Part 1: %A" (solve input solver1)
-    printfn "Part 2: %A" (solve input solver2)
+    printfn "Part 1: %A" (solve input (["*",1;"+",1] |> Map.ofList))
+    printfn "Part 2: %A" (solve input (["*",1;"+",2] |> Map.ofList))
     0
