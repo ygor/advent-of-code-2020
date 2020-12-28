52cda684cf00e86a9f5a99216a5aa7a09f3d2ea2
Ygor Geurts <ygor.geurts@gmail.com>
2020-12-28 23:35:13 +0100

    day19b

--- day19/Program.fs
+++ day19/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System.IO
+open System.Text.RegularExpressions
 open Extensions
 
 type Rule =
@@ -11,8 +12,7 @@ let input =
     |> String.split "\n\n"
     |> List.unpack2
 
-let parseIds input =
-    input |> String.split " " |> List.map int |> Ids
+let parseIds input = input |> String.split " " |> List.map int |> Ids
 
 let parseRule input =
     match input with
@@ -27,36 +27,35 @@ let rules =
     |> List.map (parseRule)
     |> Map.ofList
 
-let flatten (rules: Map<int, Rule>) =
-    let rec loop rule cont =
-        match rule with
-        | Value value -> cont [ value ]
-        | Ids (id :: ids) ->
-            loop rules.[id] (fun acc ->
-                loop (Ids ids) (fun acc' ->
-                    List.allPairs acc acc'
-                    |> List.map (fun (xs, ys) -> xs + ys)
-                    |> cont))
-        | Ids [] -> cont [ "" ]
-        | Or (r1, r2) -> loop r1 (fun r1acc -> loop r2 (fun r2acc -> cont (r1acc @ r2acc)))
+let build (rules: Map<int, Rule>) maxLength =
+    let rec loop length = function
+        | Value value -> value
+        | Ids ids ->
+            if length > maxLength then ""
+            else
+                ids
+                |> List.map (fun id -> loop (length + ids.Length) rules.[id])
+                |> List.reduce (+)
+        | Or (r1, r2) -> "(" + loop length r1 + "|" + loop length r2 + ")"
 
-    loop rules.[0] id
-
+    "^" + loop 0 rules.[0] + "$"
+    
 let rules2 =
     rules
     |> Map.add 8 (Or(Ids [ 42 ], Ids [ 42; 8 ]))
     |> Map.add 11 (Or(Ids [ 42; 31 ], Ids [ 42; 11; 31 ]))
 
-let run rules =
-    let rules' = flatten rules
-
-    snd input
-    |> String.split "\n"
-    |> List.filter (fun s -> List.contains s rules')
+let validate input (rules: Map<int, Rule>) =
+    let messages = input |> String.split "\n"
+    let maxLength = messages |> List.map (String.length) |> List.max
+    let regex = build rules maxLength
+    
+    messages
+    |> List.filter (fun s -> Regex.IsMatch(s, regex))
     |> List.length
 
 [<EntryPoint>]
 let main _ =
-    printfn "Part 1: %A" (run rules)
-//    printfn "Part 2: %A" (run rules2)
+    printfn "Part 1: %A" (validate (snd input) rules)
+    printfn "Part 2: %A" (validate (snd input) rules2)
     0
