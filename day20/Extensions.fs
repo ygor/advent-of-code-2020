80b22a06de2ea33c9b6933091bcf47d6b23b3e05
Ygor Geurts <ygor.geurts@gmail.com>
2021-01-09 23:53:07 +0100

    day20b

--- day20/Extensions.fs
+++ day20/Extensions.fs
@@ -3,7 +3,7 @@ module Extensions
 module String =
     let split (sep: string) (value: string) = value.Split(sep) |> List.ofArray
 
-module List =    
+module List =
     let unique list = list |> Set.ofList |> List.ofSeq
 
 module Matrix =
@@ -13,7 +13,24 @@ module Matrix =
             List.map List.head matrix
             :: transpose (List.map List.tail matrix)
         | _ -> []
-    
+
+module Array2D =
+    let toList array =
+        [ 0 .. (Array2D.length2 array) - 1 ]
+        |> Seq.fold (fun list' y ->
+            [ 0 .. (Array2D.length1 array) - 1 ]
+            |> Seq.fold (fun list x ->
+                if x = 0 then
+                    list @ [ [ array.[x, y] ] ]
+                else
+                    list.[0..(list.Length - 2)]
+                    @ [ List.last list @ [ array.[x, y] ] ]) list') []
+
+    let fromList (list: 'a list list) =
+        if list.IsEmpty
+        then Array2D.zeroCreate 0 0
+        else Array2D.init (list.Head.Length) list.Length (fun x y -> list.[y].[x])
+
 module Fun =
     let repeat n fn =
         match n with
--- day20/Program.fs
+++ day20/Program.fs
@@ -1,61 +1,162 @@
-﻿open System.IO
+﻿open System
+open System.IO
 open System.Numerics
 open Extensions
 
-type Tile = char list list
+type Rows = char list list
+type TileId = bigint * int
+type Tiles = Map<TileId, Rows>
+type Adjacents = Map<TileId, Map<TileId, int list>>
 
-let tiles =
+let tiles: Tiles =
     File.ReadAllText("input.txt")
     |> String.split "\n\n"
     |> List.map (fun text ->
         let lines = String.split "\n" text
-        (List.head lines).Substring(5, 4)
-        |> BigInteger.Parse,
-        List.tail lines |> List.map List.ofSeq)
+        let id = BigInteger.Parse ((List.head lines).Substring(5, 4))
+        (id, 0), List.tail lines |> List.map List.ofSeq)
+    |> Map.ofList
 
-let flipY (tile: Tile) = tile |> List.rev
+let seaMonster =
+    File.ReadLines("sea_monster.txt")
+    |> Seq.map Seq.toList
+    |> Seq.toList
 
-let rotate (tile: Tile) = tile |> Matrix.transpose |> flipY
+let flipY (rows: Rows) = rows |> List.rev
 
-let group =
+let rotate (rows: Rows) = rows |> Matrix.transpose |> flipY
+
+let transformations =
     [ 0 .. 3 ]
     |> List.collect (fun i ->
         let rotate' = Fun.repeat i rotate
         [ rotate'; rotate' >> flipY ])
 
-let variants =
+let variants: Tiles =
     tiles
-    |> List.collect (fun (id, tile) -> group |> List.map (fun f -> id, f tile))
+    |> Map.toList
+    |> List.collect (fun ((id, _), rows) ->
+        transformations
+        |> List.mapi (fun i f -> (id, i), f rows))
+    |> Map.ofList
 
-let borders (tile: Tile) =
-    [ List.head tile
-      tile |> List.map List.last
-      List.last tile
-      tile |> List.map List.head ]
+let borders (rows: Rows) =
+    [ List.head rows
+      rows |> List.map List.last
+      List.last rows
+      rows |> List.map List.head ]
+    |> List.indexed
 
-let adjacents =
+let adjacents: Adjacents =
     variants
-    |> List.map (fun (vId, variant) ->
-        let vBorders = borders variant
-        vId,
+    |> Map.map (fun (id, _) rows ->
+        let vBorders = borders rows
         variants
-        |> List.filter (fun (id, _) -> id <> vId)
-        |> List.map (fun (id, tile) ->
-            id,
-            borders tile
-            |> List.indexed
-            |> List.filter (fun (i, border) -> vBorders.[(i + 2) % 4] = border)
-            |> List.map snd)
-        |> List.filter (fun (_, borders) -> (not << List.isEmpty) borders))
+        |> Map.filter (fun (id', _) _ -> id <> id')
+        |> Map.map (fun _ rows' ->
+            borders rows'
+            |> List.filter (fun (i, border) -> snd vBorders.[(i + 2) % 4] = border)
+            |> List.map fst)
+        |> Map.filter (fun _ edges -> edges.IsEmpty |> not))
+
+let corners: Adjacents =
+    adjacents
+    |> Map.filter (fun _ neighbours -> neighbours.Count = 2)
+
+let topLeft =
+    corners
+    |> Map.findKey (fun _ neighbours ->
+        neighbours
+        |> Map.toList
+        |> List.map (snd >> Set.ofList)
+        |> List.reduce Set.union
+        |> (=) ([ 0; 3 ] |> Set.ofList))
+
+let neighbour tileId edge =
+    adjacents.[tileId]
+    |> Map.findKey (fun _ edges -> List.contains ((edge + 2) % 4) edges)
+
+let crop (rows: Rows) =
+    rows.[1..(rows.Length - 2)]
+    |> List.map (fun row -> row.[1..(row.Length - 2)])
+
+let gridSize = float tiles.Count |> Math.Sqrt |> int
+
+let assemble (grid: Map<int * int, TileId>) =
+    let croppedTileSize = variants.[topLeft].Length - 2
+    let length = gridSize * croppedTileSize
+
+    Array2D.init length length (fun x y ->
+        let x', y' = x / croppedTileSize, y / croppedTileSize
+        let rows = crop variants.[grid.[(x', y')]]
+        rows.[y % croppedTileSize].[x % croppedTileSize])
+
+let image =
+    [ 0 .. gridSize - 1 ]
+    |> Seq.fold (fun grid x ->
+        [ 0 .. gridSize - 1 ]
+        |> Seq.fold (fun (grid': Map<int * int, TileId>) y ->
+            let neighbour =
+                match (x, y) with
+                | (0, 0) -> topLeft
+                | (0, y) -> neighbour grid'.[0, y - 1] 2
+                | (x, y) -> neighbour grid'.[x - 1, y] 1
+
+            grid'.Add((x, y), neighbour)) grid) Map.empty<int * int, TileId>
+    |> assemble
+
+let imageVariants =
+    let rows = image |> Array2D.toList
+    transformations
+    |> List.map (fun f -> f rows)
+    |> List.map Array2D.fromList
+
+let isSeaMonsterAt x y (image: char [,]) =
+    [ 0 .. seaMonster.Length - 1 ]
+    |> Seq.fold (fun found' y' ->
+        [ 0 .. seaMonster.Head.Length - 1 ]
+        |> Seq.fold (fun found x' ->
+            match seaMonster.[y'].[x'] with
+            | '#' ->
+                let x'', y'' = x + x', y + y'
+                found
+                && x'' < (Array2D.length1 image)
+                && y'' < (Array2D.length2 image)
+                && image.[x'', y''] = '#'
+            | _ -> found) found') true
+
+let countSeaMonsters (image: char [,]) =
+    [ 0 .. (Array2D.length1 image) - 1 ]
+    |> Seq.fold (fun count' x ->
+        [ 0 .. (Array2D.length2 image) - 1 ]
+        |> Seq.fold (fun count y -> if isSeaMonsterAt x y image then count + 1 else count) count') 0
+
+let countHashes (rows: char [,]) =
+    rows
+    |> Array2D.toList
+    |> List.collect id
+    |> List.filter ((=) '#')
+    |> List.length
+
+let numSeaMonsterHashes = countHashes (seaMonster |> Array2D.fromList)
 
 let part1 =
-    adjacents
-    |> List.filter (fun (_, tiles) -> List.length tiles = 2)
-    |> List.map fst
+    corners
+    |> Map.toList
+    |> List.map (fst >> fst)
     |> List.unique
     |> Seq.reduce (*)
+
+let part2 =
+    imageVariants
+    |> List.map (fun rows ->
+        let monsters = countSeaMonsters rows
+        monsters,
+        countHashes rows - (monsters * numSeaMonsterHashes)) 
+    |> List.maxBy fst
 
 [<EntryPoint>]
 let main _ =
     printfn "Part 1: %A" part1
+    printfn "Part 2: %A" part2
     0
