6f83be89964a862383c24d9c8e7093d3a80c1591
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-02 09:38:52 +0100

    renaming

--- day23/Extensions.fs
+++ day23/Extensions.fs
@@ -1,5 +1,10 @@
 module Extensions
-
+    
+module List =
+    let repeat n fn = List.init n (fun _ -> fn) |> List.reduce (>>)
+    
 module Map =
     let removeMany keys map =
         keys |> List.fold (fun map' key -> Map.remove key map') map
+
+    
