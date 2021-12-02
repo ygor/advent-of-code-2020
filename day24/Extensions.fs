cc28491b01b9e41a0f731b065c20013635d44814
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-02 21:36:42 +0100

    day24 wip

--- day24/Extensions.fs
+++ day24/Extensions.fs
@@ -1,1 +1,9 @@
 module Extensions
+
+module Map =
+    let join (p: Map<'a,'b>) (q: Map<'a,'b>) = 
+        Seq.concat [ (Map.toSeq p) ; (Map.toSeq q) ] |> Map.ofSeq    
+
+module Func =
+    let repeat n repeater state =
+        [ 1 .. n ] |> List.fold (fun state' _ -> repeater state') state
--- day24/Program.fs
+++ day24/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System.IO
 open System.Text.RegularExpressions
+open Extensions
 
 type Color = | Black | White
 
@@ -25,16 +26,57 @@ let adjacents tile =
     vectors
     |> Map.toList |> List.map (snd >> fun (x, y) -> (x + fst tile, y + snd tile))
 
-let part1 =
+let newFloor =
     instructions
-    |> Seq.fold (fun floor instruction ->
+    |> Seq.fold (fun floor' instruction ->
         instruction
         |> List.fold (fun cx vector -> (fst cx + fst vector, snd cx + snd vector)) (0.0, 0.0)
-        |> flip floor) Map.empty
+        |> flip floor') Map.empty
+    
+let countBlack floor =
+    floor
     |> Map.filter (fun _ color -> color = Black)
     |> Map.count
 
+let part1 = countBlack newFloor
+
+let whiteTiles floor =
+    floor
+    |> Map.filter (fun tile color ->
+        color = Black &&
+        adjacents tile
+        |> List.filter (fun cx -> Map.containsKey cx floor && floor.[cx] = Black)
+        |> List.length
+        |> (fun count -> count = 0 || count > 2))
+    |> Map.map (fun _ _ -> White) 
+ 
+let blackTiles floor =
+    let keys = Map.keys floor
+    let maxX, minX = Seq.maxBy fst keys |> fst, Seq.minBy fst keys |> fst
+    let maxY, minY = Seq.maxBy snd keys |> snd, Seq.minBy snd keys |> snd
+    
+    List.allPairs [minX - 1.0 .. maxX + 1.0] [minY - 1.0 .. maxY + 1.0]
+    |> List.filter (fun tile ->
+        (not (Map.containsKey tile floor) || floor.[tile] = White) &&
+        adjacents tile
+        |> List.filter (fun cx -> Map.containsKey cx floor && floor.[cx] = Black)
+        |> List.length
+        |> (fun count -> count = 2))
+    |> List.map (fun cx -> (cx, Black))
+    |> Map.ofList
+
+let dayFlip floor =
+    floor
+    |> Map.join (whiteTiles floor)
+    |> Map.join (blackTiles floor)
+
+let part2 =
+    newFloor
+    |> Func.repeat 100 dayFlip
+    |> countBlack
+    
 [<EntryPoint>]
 let main _ =
     printfn $"Part 1: %i{part1}"
+    printfn $"Part 2: %i{part2}"
     0
