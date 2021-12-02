6f2f9013a2bf2056258268c714e1ad6e8af6ef9d
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-02 20:37:38 +0100

    day24 part 1

--- day24/Program.fs
+++ day24/Program.fs
@@ -1,8 +1,38 @@
 ﻿open System.IO
+open System.Text.RegularExpressions
 
-let tiles =
-    File.ReadAllText("input.txt")
+type Color = | Black | White
+
+let parse =
+    function
+    | "e" -> (1.0, 0.0)
+    | "se" -> (0.5, -1.0)
+    | "sw" -> (-0.5, -1.0)
+    | "w" -> (-1.0, 0.0)
+    | "nw" -> (-0.5, 1.0)
+    | "ne" -> (0.5, 1.0)
+    | str -> failwithf $"Invalid input %s{str}"
+
+let instructions =
+    File.ReadAllLines("input.txt")
+    |> Seq.map
+        (fun line -> seq { for m in Regex.Matches(line, "e|se|sw|w|nw|ne") do yield parse m.Value } |> Seq.toList)
+
+let flip floor tile =
+    if Map.containsKey tile floor
+    then Map.add tile (if floor.[tile] = Black then White else Black) floor
+    else Map.add tile Black floor
+
+let part1 =
+    instructions
+    |> Seq.fold (fun floor instruction ->
+        instruction
+        |> List.fold (fun cx vector -> (fst cx + fst vector, snd cx + snd vector)) (0.0, 0.0)
+        |> flip floor) Map.empty
+    |> Map.filter (fun _ color -> color = Black)
+    |> Map.count
 
 [<EntryPoint>]
 let main _ =
+    printfn $"Part 1: %i{part1}"
     0
